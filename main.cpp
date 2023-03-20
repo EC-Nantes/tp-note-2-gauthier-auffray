8a3810951c7fa7b6b2c8d9b18b5b6175c83af00f
AUFFRAY Jean <jean.auffray@altran.com>
2023-03-20 22:16:47 +0100

    gestion du main pour jouer une partie

--- main.cpp
+++ main.cpp
@@ -26,29 +26,8 @@ int main() {
     std::cout << "nb joueur reel : " << unsigned(nb_joueur) << "\n";
     Plateau_t plateau(nb_joueur);
     std::cout << plateau;
-    uint16_t c;
-    uint16_t p;
-    std::vector<Joueur_t*> joueurs = plateau.getJoueurs();
-    plateau.find_tortue(joueurs[4], &c, &p);
-    std::cout << "Le joueur " << joueurs[4]->getCouleurS();
-    std::cout << " est sur la case " << c << " a la position " << p +1<< "\n";
-    std::vector<Joueur_t*> joueurs_2 = plateau.getDerniers();
-    for(auto& temp : joueurs_2) {
-        std::cout << *temp << "\n";
+    while(!plateau.tourDeJeu()) {
     }
-
-    // TypeAction_t AVANCER;
-    // uint8_t case1 = 1;
-    // uint8_t case2 = 2;
-
-    // Joueur_t j1(ROUGE_J);
-    // j1.addCarte(new Carte_t(ROUGE_C, AVANCER, case2));
-    // j1.addCarte(new Carte_t(BLEU_C, AVANCER, case2));
-    // j1.addCarte(new Carte_t(VERT_C, RECULER, case1));
-    // j1.addCarte(new Carte_t(NEUTRE_C, DERNIER, case2));
-    // j1.addCarte(new Carte_t(NEUTRE_C, AVANCER, case1));
-
-    // j1.choixCarte();
-
+    std::cout << "\n********** End tortuga **********\n";
     return 0;
 }
